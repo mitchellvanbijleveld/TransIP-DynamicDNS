17ab57883130c8fddd710889835c088ca98585e4
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2022-05-27 17:40:50 +0200

    Added newlines when printing text to terminal

--- UpdateSingleRecordDNS.php
+++ UpdateSingleRecordDNS.php
@@ -5,6 +5,7 @@ use Transip\Api\Library\Entity\Domain\DnsEntry;
 // Get Latest Saved IP.
 $IP_Old = file_get_contents("LatestFetchedIPAddress.txt");
 echo $IP_Old;
+echo "\n";
 
 //Run Script to Get Updated IP.
 shell_exec('sh GetExternalIP.sh');
@@ -12,9 +13,11 @@ shell_exec('sh GetExternalIP.sh');
 // Get Latest Fetched And Saved IP.
 $IP_New = file_get_contents("LatestFetchedIPAddress.txt");
 echo $IP_New;
+echo "\n";
 
 if ($IP_New <> $IP_Old) {
   echo 'IP is different. Updating the A Record...';
+  echo "\n";
 
 require_once (__DIR__ . '/TransIP-Authenticate-API.php');
 
@@ -29,7 +32,8 @@ $dnsEntry->setContent(trim($IP_New));
 
 // Apply entry
 $api->domainDns()->updateEntry($domainName, $dnsEntry);
-  	
+
 } else {
-	echo 'IP is the same as current A Record.';
+	echo "IP is the same as current A Record.";
+  echo "\n";
 }
